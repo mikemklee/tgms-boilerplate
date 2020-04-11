cf1beda812ef62d0f0b0541635029d86f82b7854
Mike Lee <mikemunkyu.lee@gmail.com>
2020-04-11 18:08:40 -0400

    Hookup Stripe API and add createSubscription mutation

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -2,6 +2,7 @@ import { IResolvers } from 'apollo-server-express';
 import * as bcrypt from 'bcryptjs';
 
 import { User } from './entity/User';
+import { stripe } from './stripe';
 
 export const resolvers: IResolvers = {
   Query: {
@@ -34,6 +35,35 @@ export const resolvers: IResolvers = {
       }
 
       req.session.userId = user.id;
+
+      return user;
+    },
+    createSubscription: async (_, { source }, { req }) => {
+      if (!req.session || !req.session.userId) {
+        throw new Error('not authenticated');
+      }
+
+      const user = await User.findOne(req.session.userId);
+
+      if (!user) {
+        throw new Error();
+      }
+
+      // create new customer
+      const customer = await stripe.customers.create({
+        email: user.email,
+        source: source,
+      });
+
+      // subscribe customer to plan
+      stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ plan: process.env.STRIPE_PLAN }],
+      });
+
+      user.stripeId = customer.id;
+      user.type = 'paid';
+      await user.save();
 
       return user;
     },
