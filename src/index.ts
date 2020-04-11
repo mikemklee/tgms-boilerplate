c5545d2128df9cebf72b1b54ea1ede7d25a6a0d4
Mike Lee <mikemunkyu.lee@gmail.com>
2020-04-11 14:12:42 -0400

    Add User entity and write mutations for register and login

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,25 @@
 // import 'reflect-metadata';
-// import { createConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
 import * as express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
 
-// import { User } from './entity/User';
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-// createConnection()
-//   .then(async (connection) => {
-//     console.log('Inserting a new user into the database...');
-//     const user = new User();
-//     user.firstName = 'Timber';
-//     user.lastName = 'Saw';
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log('Saved a new user with id: ' + user.id);
+  await createConnection();
 
-//     console.log('Loading users from the database...');
-//     const users = await connection.manager.find(User);
-//     console.log('Loaded users: ', users);
+  const app = express();
 
-//     console.log('Here you can setup and run express/koa/any other framework.');
-//   })
-//   .catch((error) => console.log(error));
+  server.applyMiddleware({ app });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
 
-const app = express();
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+startServer();
