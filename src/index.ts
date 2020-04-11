25c7d3696436cd274dd7f213da0058bf3bf81ceb
Mike Lee <mikemunkyu.lee@gmail.com>
2020-04-11 13:54:36 -0400

    Test Apollo server spin up

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+// import 'reflect-metadata';
+// import { createConnection } from 'typeorm';
+import * as express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './typeDefs';
+import { resolvers } from './resolvers';
 
-createConnection().then(async connection => {
+// import { User } from './entity/User';
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+// createConnection()
+//   .then(async (connection) => {
+//     console.log('Inserting a new user into the database...');
+//     const user = new User();
+//     user.firstName = 'Timber';
+//     user.lastName = 'Saw';
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log('Saved a new user with id: ' + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+//     console.log('Loading users from the database...');
+//     const users = await connection.manager.find(User);
+//     console.log('Loaded users: ', users);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+//     console.log('Here you can setup and run express/koa/any other framework.');
+//   })
+//   .catch((error) => console.log(error));
 
-}).catch(error => console.log(error));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
