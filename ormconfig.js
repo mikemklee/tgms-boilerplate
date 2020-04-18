fec62e3a44b3e7072861fb4597ef0f889c7fcdf4
Mike Lee <mikemunkyu.lee@gmail.com>
2020-04-18 16:52:35 -0400

    Modify ormconfig file to respect node env

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,18 @@
+const fileExtension = process.env.NODE_ENV === 'development' ? 'ts' : 'js';
+
+const entitiesDir =
+  process.env.NODE_ENV === 'development' ? 'server/entity' : 'dist/entity';
+
+const migrationsDir =
+  process.env.NODE_ENV === 'development'
+    ? 'server/migration'
+    : 'dist/migration';
+
+const subscribersDir =
+  process.env.NODE_ENV === 'development'
+    ? 'server/subscriber'
+    : 'dist/subscriber';
+
 module.exports = {
   type: 'mongodb',
   database: process.env.MONGODB_NAME,
@@ -6,12 +21,12 @@ module.exports = {
   useUnifiedTopology: true,
   synchronize: true,
   logging: true,
-  entities: ['server/entity/**/*.ts'],
-  migrations: ['server/migration/**/*.ts'],
-  subscribers: ['server/subscriber/**/*.ts'],
+  entities: [`${entitiesDir}/**/*.${fileExtension}`],
+  migrations: [`${migrationsDir}/**/*.${fileExtension}`],
+  subscribers: [`${subscribersDir}/**/*.${fileExtension}`],
   cli: {
-    entitiesDir: 'server/entity',
-    migrationsDir: 'server/migration',
-    subscribersDir: 'server/subscriber',
+    entitiesDir: entitiesDir,
+    migrationsDir: migrationsDir,
+    subscribersDir: subscribersDir,
   },
 };
